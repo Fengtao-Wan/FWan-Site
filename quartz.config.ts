b17f32760feb8ce66afd9fa916ffa54e13b7a2dc
Fengtao-Wan <fengtaowan@gmail.com>
2026-01-18 20:08:18 +0100

    Quartz sync: Jan 18, 2026, 8:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "fengtao-wan.github.io/FWan-Site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
--- quartz/components/SideNav.tsx
+++ quartz/components/SideNav.tsx
@@ -7,7 +7,21 @@ const SideNav: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
       
       <ul style={{ listStyle: "none", padding: 0, margin: "2rem 0 0 0" }}>
         
+        {/* LINK 1: Research */}
+        <li style={{ marginBottom: "0.5rem" }}>
+          {/* REMOVED "/content" and kept lowercase */}
+          <a href="/FWan-Site/research" style={{ color: "var(--secondary)", textDecoration: "none" }}>
+            Research
+          </a>
+        </li>
 
+        {/* LINK 2: CV */}
+        <li style={{ marginBottom: "0.5rem" }}>
+          {/* REMOVED "/content" and changed "CV" to "cv" */}
+          <a href="/FWan-Site/cv" style={{ color: "var(--secondary)", textDecoration: "none" }}>
+            CV
+          </a>
+        </li>
 
       </ul>
     </div>
